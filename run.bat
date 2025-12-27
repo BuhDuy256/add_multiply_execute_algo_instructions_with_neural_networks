120b5f49b078bfd157cf105b9562e612b6d7e25a
BuhDuy <nbduy23@clc.fitus.edu.vn>
2025-12-28 00:14:41 +0700

    update

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 conda activate exact_learning
 cd training
-python experiment.py --k-list 5 10 --trials 5 --epochs 1250 --target-acc 0.95
+python experiment.py --k-list 5 10 15 --trials 1 --epochs 10000 --target-acc 0.999
